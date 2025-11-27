c8f0385b0a2f9e9c7c7d546ce512b40c1bd44b29
Nupur Dashputre <nupurdashputre@Mac.lan>
2025-11-27 13:03:25 -0800

    scans and vitals visible for all roles

--- routes/vitals.js
+++ routes/vitals.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { protect, authorize, scope } = require('../middleware/authMiddleware');
+const { protect, authorize, scope, patientCheck } = require('../middleware/authMiddleware');
 const router = express.Router();
 const {
   createVital,
@@ -18,45 +18,7 @@ router.delete('/:id', protect, authorize('admin', 'doctor', 'manager', 'nurse'),
 
 
 //to display line chart + vitals history
-router.get('/history/:patientId', protect, async (req, res, next) => {
-
-  const allowedRoles = ['admin', 'doctor', 'manager', 'nurse'];
-  const requestedPatientId = req.params.patientId;
-
-  // If logged-in user is a patient
-  if (req.user.role === 'patient') {
-    const { Patient } = require('../models');
-
-    // Find patient record belonging to this user
-    const patient = await Patient.findOne({
-      where: { userId: req.user.id }
-    });
-
-    if (!patient) {
-      return res.status(404).json({
-        error: 'No patient record associated with this login.'
-      });
-    }
-
-    // Check if patient is trying to access someone else’s records
-    if (patient.userId.toString() !== requestedPatientId) {
-      return res.status(403).json({
-        error: 'Patients can only view their own vitals history.'
-      });
-    }
-
-    // Allowed → pass to controller
-    return getVitalsHistoryByPatientId(req, res);
-  }
-
-  // If user is medical staff → verify role
-  if (!allowedRoles.includes(req.user.role)) {
-    return res.status(403).json({ error: 'Unauthorized' });
-  }
-
-  // Staff allowed → send to controller
-  return getVitalsHistoryByPatientId(req, res);
-});
+router.get("/history/:patientId", protect, patientCheck, authorize("admin", "doctor", "manager", "nurse", "patient"), getVitalsHistoryByPatientId);
 
 
 module.exports = router;
