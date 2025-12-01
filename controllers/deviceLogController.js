719fd6df6b5c08da4bb9e2ff32daab71e5090366
Nupur Dashputre <nupurdashputre@Mac.lan>
2025-11-30 21:41:54 -0800

    update deviceLog controller to send device name

--- controllers/deviceLogController.js
+++ controllers/deviceLogController.js
@@ -1,4 +1,4 @@
-const { DeviceLog, User } = require('../models');
+const { DeviceLog, User, Admission, ComplianceAlert, Patient, Staff, Camera, Scan } = require('../models');
 const { Op } = require('sequelize');
 const { sequelize } = require('../config/db');
 const { logger } = require('../utils/logger');
@@ -11,16 +11,73 @@ const createDeviceLog = async (req, res) => {
   logger.info(`[${endpoint}] Request to create device log received from ${userEmail}`);
 
   try {
+    const { entity } = req.body;
+
+    //If entity exists and has an id, fetch its name
+    if (entity?.type && entity?.id) {
+      let entityRecord;
+
+      switch (entity.type.toLowerCase()) {
+        case 'patient':
+          entityRecord = await Patient.findOne({ where: { id: entity.id } });
+          break;
+        case 'staff':
+          entityRecord = await Staff.findOne({ where: { id: entity.id } });
+          break;
+        case 'scan':
+          entityRecord = await Scan.findOne({ where: { id: entity.id } });
+          break;
+        case 'alert':
+          entityRecord = await ComplianceAlert.findOne({ where: { id: entity.id } });
+          break;
+        case 'task':
+          entityRecord = await Task.findOne({ where: { id: entity.id } });
+          break;
+        case 'admission':
+          entityRecord = await Admission.findOne({ where: { id: entity.id } });
+          break;
+        case 'device':
+          entityRecord = await Camera.findOne({ where: { id: entity.id } });
+          break;
+      }
+
+      if (entityRecord) {
+        req.body.entity = {
+          type: entity.type,
+          name: entityRecord.name
+        };
+      } else {
+        req.body.entity = {
+          type: entity.type,
+          name: 'Unknown'
+        };
+      }
+    }
+
+    //Create the device log
     const log = await DeviceLog.create(req.body);
+
+    const logWithUser = await DeviceLog.findOne({
+      where: { id: log.id },
+      include: [
+        {
+          model: User,
+          as: 'user',
+          attributes: ['id', 'name', 'email'] 
+        }
+      ]
+    });
+
     logger.info(`[${endpoint}] Device log created successfully`, { id: log.id, user: userEmail });
-    res.status(201).json(log);
+    res.status(201).json(logWithUser);
   } catch (err) {
     logger.error(`[${endpoint}] Error creating device log: ${err.message}`, { stack: err.stack });
     res.status(400).json({ error: err.message });
   }
 };
 
-// Get all device logs
+
+//Get all device logs
 const getDeviceLogs = async (req, res) => {
   const endpoint = 'getDeviceLogs';
   const userEmail = req.user?.email || 'unknown';
